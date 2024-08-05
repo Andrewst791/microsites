982f10885b2c5f74d5d33648db2a1317d4e4d3fa
=Andres Guerrero Guerrero <=andres.guerrero@caminos.com.co>
2024-08-05 01:40:27 -0500

    feat: model and service payment

---
+++ database/migrations/2024_08_05_063602_create_payments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Constants\PaymentStatus;
+use App\Constants\PaymentGateway;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference')->unique();
+            $table->string('description', 100);
+            $table->unsignedBigInteger('amount');
+            $table->foreignId('currency_id')->constrained();
+            $table->enum('status', PaymentStatus::toArray());
+            $table->enum('gateway', PaymentGateway::toArray());
+            $table->unsignedInteger('process_identifier')->nullable();
+            $table->dateTime('expiration')->nullable();
+            $table->json('fields_data')->nullable();
+            $table->foreignId('microsite_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
